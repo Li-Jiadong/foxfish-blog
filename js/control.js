e25afe3e06e74225b69d82beb462ff9904465a77
fish <ljd200102@163.com>
2020-01-22 13:04:23 +0800

    add leftmenu

--- js/control.js
+++ js/control.js
@@ -1,3 +1,4 @@
+var leftmenuopen=false;
 window.onload=function(){
     var imsize=document.getElementById('headimg');
     var w=Number(imsize.clientWidth);
@@ -7,4 +8,20 @@ window.onresize = function(){
     var imsize=document.getElementById('headimg');
     var w=Number(imsize.clientWidth);
     imsize.style.height=w/16*8+'px';
+}
+function showleftmenu(){
+    var leftmenu=document.getElementById('leftmenu');
+    var mainpage=document.getElementById('mainpage');
+    //console.log('oooook');
+    if(leftmenuopen){
+        //already open
+        leftmenuopen=false;
+        leftmenu.style.left='-300px';
+        mainpage.style.left='0px';
+    }else{
+        //haven't open
+        leftmenuopen=true;
+        leftmenu.style.left='0px';
+        mainpage.style.left='300px';
+    }
 }
