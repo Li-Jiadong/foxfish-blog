95f60ff3fcdb167541db8a05853f8ea070aa9851
fish <ljd200102@163.com>
2020-01-22 18:14:36 +0800

    add return function

--- js/control.js
+++ js/control.js
@@ -12,15 +12,26 @@ window.onresize = function(){
 function showleftmenu(){
     var leftmenu=document.getElementById('leftmenu');
     var mainpage=document.getElementById('mainpage');
+    var returnpage=document.getElementById('leftmenu_right');
     //console.log('oooook');
     if(leftmenuopen){
         //already open
         leftmenuopen=false;
+        returnpage.style.display='none';
+        document.body.style.overflow='auto';
+        //动画
         leftmenu.style.left='-300px';
         mainpage.style.left='0px';
     }else{
         //haven't open
         leftmenuopen=true;
+        //alert(document.documentElement.clientHeight);
+        leftmenu.style.height=document.documentElement.clientHeight+'px';
+        returnpage.style.display='block';
+        returnpage.style.height=document.documentElement.clientHeight+'px';
+        returnpage.style.width=document.documentElement.clientWidth-300+'px';
+        document.body.style.overflow='hidden';
+        //动画
         leftmenu.style.left='0px';
         mainpage.style.left='300px';
     }
